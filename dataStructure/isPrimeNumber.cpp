27def84a4ddc66f1a12aedab6d07f383dbd76e8c
Yuelei <593845439@qq.com>
2021-01-02 18:28:24 +0800

    is or not prime number

---
+++ dataStructure/isPrimeNumber.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+/*
+ *判断一个数是否为 prime number
+ */
+
+
+void isPrimeNumber(int value) {
+	int i;
+	for (i = 2; i < value; i++)
+		if (value%i == 0) {
+			std::cout << value << " is not prime number" << "\n";
+			break;
+			
+		}
+	
+	if (i == value)std::cout << value << " is prime number" << "\n";
+
+}
+
+int main() {
+	int value;
+	std::cin >> value;
+	isPrimeNumber(value);
+}

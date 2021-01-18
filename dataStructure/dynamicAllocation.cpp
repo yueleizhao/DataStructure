872bc1c093f5211c0e524a9e3565e1128687b719
Yuelei <593845439@qq.com>
2021-01-18 21:13:21 +0800

    dynamic allocation

---
+++ dataStructure/dynamicAllocation.cpp
@@ -0,0 +1,52 @@
+
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h> 
+using namespace std;
+/**
+ * 动态内存分配
+ */
+
+
+
+int main() {
+	int n = 0, m = 0;
+
+	cin >> n >> m;
+
+	int* arr1 = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr1[i] = i;
+		printf("%d", arr1[i]);
+	}
+
+	int* arr2 = NULL;
+	arr2 = (int*)malloc(sizeof(int)*n);
+	for (int i = 0; i < n; i++) {
+		arr2[i] = i;
+		printf("%d", arr2[i]);
+	}
+
+	int(*arr3)[n] = (int(*)[n])malloc(sizeof(int)*m*n);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+
+			arr3[i][j] = i + j;
+			printf("%d", arr3[i][j]);
+		}
+
+
+	int **arr4 = (int**)malloc(sizeof(int*)*n);
+	for (int i = 0; i < n; i++) {
+		arr4[i] = (int*)malloc(sizeof(int)*m);
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+
+			arr4[i][j] = i + j;
+			printf("%d", arr4[i][j]);
+
+		}
+
+	return 0;
+}

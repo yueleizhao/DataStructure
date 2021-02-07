45e750cc035a5b5302e8cac5adc58b349bfe2574
Yuelei <593845439@qq.com>
2021-02-07 09:22:09 +0800

    将一个小数四舍五入保留两位小数

---
+++ dataStructure/basic/round_to_the_nearest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+/*
+ * 将一个小数四舍五入保留两位小数
+ */
+
+double roundToTheNearest(double value) {
+	int temp = (int)(value * 1000 + 5) / 10;
+
+	return (double)temp / 100.0;
+}
+
+int main() {
+	double value;
+	std::cin >> value;
+	std::cout << roundToTheNearest(value);
+	return 0;
+}

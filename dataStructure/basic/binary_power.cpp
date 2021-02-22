0507acd691d1d06d7e4f3f7ca6877a62543580e9
Yuelei <593845439@qq.com>
2021-02-22 16:02:33 +0800

    [ 快速幂，二分幂 ]

--- dataStructure/basic/binary_power.cpp
+++ dataStructure/basic/binary_power.cpp
@@ -10,18 +10,34 @@ using namespace std;
  */
 
  //求解a^b % m
-
-long long binary_power(long a, int long b, int long m) {
+ //递归形式
+long long binary_power1(long a, int long b, int long m) {
 
 	if (b == 0) return 1;
-	if (b % 2 == 1) return a * binary_power(a, b - 1, m) % m;
+	if (b % 2 == 1) return a * binary_power1(a, b - 1, m) % m;
 	else {
-		long long temp = binary_power(a, b / 2, m);
+		long long temp = binary_power1(a, b / 2, m);
 
 		return temp * temp % m;
 
 	}
 
+}
+//非递归形式
+//例如 13=(1101)B,13 = 8+4+1 ,a^13 = a^(8+4+1)= a^8 * a^4 * a^1
+long long binary_power2(long a, int long b, int long m) {
+	long long ans = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			ans = ans * a%m;
+
+		}
+		a = a * a%m;//a的平方的表示
+
+		b /= 2;
+
+	}
+	return ans;
 }
 
 int main() {
@@ -31,10 +47,13 @@ int main() {
 	if (m == 1)//任何整数取模 1 都是0
 		return 0;
 	int ans;
-	if (a >= m)//a >= m,提前取模 
-		ans = binary_power(a%m, b, m);
-	else ans = binary_power(a, b, m);
 
-	cout << ans;
+	if (a >= m)//a >= m,提前取模
+		ans = binary_power1(a%m, b, m);
+	else ans = binary_power1(a, b, m);
+
+	cout << ans << '\n';
+
+	cout << binary_power2(a, b, m);
 	return 0;
 }
